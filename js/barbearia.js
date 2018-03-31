b79ce4c5617abc01410b30a34d0ebbd99c42cda8
Junior Ribeiro <juniors-3@hotmail.com>
2018-03-31 13:44:15 -0300

    scroll suave interno

--- js/barbearia.js
+++ js/barbearia.js
@@ -1,8 +1,19 @@
+//Menu Sanduiche
+
 $('.mobile-btn').click(function () {
     $(this).toggleClass('active');
 
     $('.aqui').toggleClass('on');
+});
 
+//Scroll Suave Interno
 
-    //    $('.mobile-menu').toggleClass('active');
+$('.menu a[href^="#"]').on('click', function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top;
+    
+    $('html, body').animate({
+        scrollTop: targetOffset
+    },500);
 });
